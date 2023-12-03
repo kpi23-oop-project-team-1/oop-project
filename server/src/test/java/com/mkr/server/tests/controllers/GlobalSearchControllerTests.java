553411e6ebd154d2780f0268c318b0f6dead88ab
Artem Litkovskyi <litkovskyi.artem@gmail.com>
2023-12-03 16:34:43 +0200

    Add GlobalSearchControllerTests, ProductControllerTests

---
+++ server/src/test/java/com/mkr/server/tests/controllers/GlobalSearchControllerTests.java
@@ -0,0 +1,84 @@
+package com.mkr.server.tests.controllers;
+
+import com.mkr.datastore.DataStore;
+import com.mkr.datastore.inMemory.InMemoryDataStore;
+import com.mkr.server.config.DataStoreConfig;
+import com.mkr.server.domain.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GlobalSearchControllerTests {
+    @TestConfiguration
+    public static class Configuration {
+        @Bean
+        @Primary
+        public DataStore testDataStore() {
+            return new InMemoryDataStore(DataStoreConfig.configuration);
+        }
+    }
+
+    @Autowired
+    private DataStore dataStore;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void beforeTest() {
+        var products = dataStore.getCollection(DataStoreConfig.products);
+
+        products.delete(p -> true);
+
+        products.insert(
+            new Product(
+                0, 0,
+                "123",
+                100,
+                5,
+                ProductCategory.DRESS,
+                ProductState.NEW,
+                ProductStatus.ACTIVE,
+                ColorId.BLACK
+        ),
+        new Product(
+                1, 1,
+                "456",
+                150,
+                5,
+                ProductCategory.SPORT,
+                ProductState.ACCEPTABLE,
+                ProductStatus.ACTIVE,
+                ColorId.WHITE
+            )
+        );
+    }
+
+    @Test
+    public void cartProductsTest() throws Exception {
+        mockMvc.perform(get("/api/searchfilterdesc"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"limitingPriceRange\":{\"start\":100,\"end\":150},\"colorIds\":[\"black\",\"white\"],\"states\":[\"acceptable\",\"new\"]}"));
+    }
+
+    @Test
+    public void getProductsTest() throws Exception {
+        mockMvc.perform(get("/api/products"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"pageCount\":1,\"totalProductCount\":2,\"products\":[{\"id\":0,\"title\":\"123\",\"imageSource\":\"/images/product/0/0\",\"price\":100,\"totalAmount\":5},{\"id\":1,\"title\":\"456\",\"imageSource\":\"/images/product/1/0\",\"price\":150,\"totalAmount\":5}]}"));
+    }
+}
---
+++ server/src/test/java/com/mkr/server/tests/controllers/ProductControllerTests.java
@@ -0,0 +1,151 @@
+package com.mkr.server.tests.controllers;
+
+import com.mkr.datastore.DataStore;
+import com.mkr.datastore.inMemory.InMemoryDataStore;
+import com.mkr.server.config.DataStoreConfig;
+import com.mkr.server.domain.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTests {
+    @TestConfiguration
+    public static class Configuration {
+        @Bean
+        @Primary
+        public DataStore testDataStore() {
+            return new InMemoryDataStore(DataStoreConfig.configuration);
+        }
+
+        @SuppressWarnings("deprecation")
+        @Primary
+        @Bean
+        public PasswordEncoder testPasswordEncoder() {
+            return NoOpPasswordEncoder.getInstance();
+        }
+    }
+
+    @Autowired
+    private DataStore dataStore;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void beforeTest() {
+        var users = dataStore.getCollection(DataStoreConfig.users);
+        var products = dataStore.getCollection(DataStoreConfig.products);
+
+        users.delete(u -> true);
+        products.delete(p -> true);
+        users.insert(createUser(0, 1));
+        users.insert(createUser(1, -1));
+
+        products.insert(
+                new Product(
+                        0, 0,
+                        "123",
+                        100,
+                        5,
+                        ProductCategory.DRESS,
+                        ProductState.NEW,
+                        ProductStatus.ACTIVE,
+                        ColorId.BLACK
+                ),
+                new Product(
+                        1, 0,
+                        "456",
+                        150,
+                        5,
+                        ProductCategory.SPORT,
+                        ProductState.ACCEPTABLE,
+                        ProductStatus.ACTIVE,
+                        ColorId.WHITE
+                )
+        );
+    }
+
+    private User createUser(int id, int commentAuthorId) {
+        var user = new CustomerTraderUser(
+                id,
+                "mail" + id + "@gmail.com",
+                passwordEncoder.encode("password")
+        );
+        user.setTelNumber("1234567891");
+        user.setFirstName("First name " + id);
+        user.setLastName("Last name " + id);
+        user.setDisplayName("Display name " + id);
+        user.setProfileDescription("Description " + id);
+
+        if (commentAuthorId >= 0) {
+            user.setComments(new Comment[]{
+                    new Comment(0, id, commentAuthorId, 5, "123 " + id, 100)
+            });
+        } else {
+            user.setComments(new Comment[0]);
+        }
+
+        user.setCartProducts(new CartProduct[]{
+                new CartProduct(0, 5)
+        });
+        user.setProducts(new Product[0]);
+
+        return user;
+    }
+
+    @Test
+    public void productInfoTest() throws Exception {
+        mockMvc.perform(get("/api/product?id=0")).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json("{\"id\":0,\"title\":\"123\",\"imageSources\":[],\"price\":100,\"totalAmount\":5,\"description\":\"\",\"comments\":[],\"category\":\"dress\",\"state\":\"new\",\"color\":\"black\",\"status\":\"active\",\"trader\":{\"id\":0,\"displayName\":\"Display name 0\"}}"));
+    }
+
+    @Test
+    public void postProductCommentTest() throws Exception {
+        mockMvc.perform(
+                post("/api/postproductcomment")
+                        .param("targetId", "0")
+                        .param("rating", "3")
+                        .param("text", "test")
+                        .with(user("mail1@gmail.com").password("password").roles("USER", "CUSTOMER_TRADER"))
+        ).andExpect(status().isOk());
+
+        var product = dataStore.getCollection(DataStoreConfig.products).data()
+                .filter(p -> p.getProductId() == 0)
+                .findFirst().orElseThrow();
+
+        assertEquals(1, product.getComments().length);
+    }
+
+    @Test
+    public void deleteProductsTest() throws Exception {
+        mockMvc.perform(
+                delete("/api/deleteproducts")
+                        .content("[0,1]")
+                        .with(user("mail0@gmail.com").password("password").roles("USER", "CUSTOMER_TRADER"))
+        ).andExpect(status().isOk());
+
+        var a = dataStore.getCollection(DataStoreConfig.products).data().toArray();
+        assertEquals(0, a.length);
+    }
+}
